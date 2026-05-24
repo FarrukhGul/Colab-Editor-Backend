ba843b92f2bb744fa4b683ee5b442624b431186e
FarrukhGul <coder.gulfarrukh@gmail.com>
2026-05-24 19:34:28 +0500

    cors adjusted

--- src/app.js
+++ src/app.js
@@ -15,9 +15,14 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: env.FRONTEND_URL || 'https://colab-editor-farrukh.vercel.app',
+    origin: [
+        'https://colab-editor-farrukh.vercel.app',
+        'http://localhost:5173'
+    ],
     credentials: true,
-}));
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 app.use(cookieParser())
 
